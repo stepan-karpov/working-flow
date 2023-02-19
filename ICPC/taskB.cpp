4ed9290a9acf266883abce37e3c6a72c51a02c93
Stepan Karpov <stepan.karpov@list.ru>
2023-02-19 11:14:38 +0300

    working flow

---
+++ ICPC/taskB.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void Init() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+}
+
+int main() {
+  Init();
+  
+  return 0;
+}
