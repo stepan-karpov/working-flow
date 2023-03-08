c5340382b7dc5bf8245b9f540cd6fa21b8a610d8
Stepan Karpov <stepan.karpov@list.ru>
2023-03-08 13:46:19 +0300

    moving some trash

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ i = 0
 while (True):
     print("jopa" + str(i))
     i += 1
+    if (i == 3):
+        break
--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-path="id"
-if [ "$1" == "-s" ] || [ "$1" == "--source" ]; then
-  echo "lol1"
-fi
-
-if [ "$2" = "-s" ] || [ "$2" = "--source" ]; then
-  echo "lol2"
-fi
+echo "jopa"
