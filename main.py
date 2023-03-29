e861fe1e252bf0eba0c2b6c9ab9623fd189538e7
Stepan Karpov <stepan.karpov@list.ru>
2023-03-29 16:55:41 +0300

    working flow

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-def append(my_list):
-  my_list = [0, 0]
-  my_list[0] = 1
-  return 2
-my_list = (0, 1)
-my_list[0] = append(my_list)
-print(my_list)
+r = open("input.txt", mode='r')
+
+ans = []
+
+for line in r.readlines():
+  words = line[30:].split()
+  for word in words:
+    c = word.strip("\n")
+    if (c.lower() != c):
+      continue
+    if (word[0] == "."):
+      continue
+    if (word[-1] == "."):
+      c = word[:-1]
+    if (c.endswith(".hlm") or c.endswith(".brhl")):
+      ans.append(c)
+
+o = open("output.txt", mode='a')
+
+for el in ans:
+  o.write(el + "\n")
+
+r.close()
