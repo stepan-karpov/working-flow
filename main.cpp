b72ba778ce027834f64b2342e1f8828af98bdc0a
Stepan Karpov <stepan.karpov@list.ru>
2023-08-24 17:22:44 +0300

    working flow

--- main.cpp
+++ main.cpp
@@ -22,74 +22,8 @@ const string ALPH = "abcdefghijklmnopqrstuvwxyz";
 
 // v2 = rand() % 100 + 1;  --- v2 in the range 1 to 100
 
-void Count1(vector<pll>& max_sequence_left, string& s) {
-  ll n = s.size();
-  ll current_zeros = 0;
-  ll current_ones = 0;
-  if (s[0] == '0') {
-    max_sequence_left[0].second = 1;
-    current_zeros = 1;
-  } else {
-    max_sequence_left[0].first = 1;
-    current_ones = 1;
-  }
-  for (int i = 1; i < n; ++i) {
-    if (s[i] == '0') {
-      current_ones = 0;
-      current_zeros += 1;
-      max_sequence_left[i].second = max(max_sequence_left[i - 1].second, current_zeros);
-    } else {
-      current_zeros = 0;
-      current_ones += 1;
-      max_sequence_left[i].first = max(max_sequence_left[i - 1].first, current_ones);
-    }
-  }
-}
-
-void Count2(vector<pll>& max_sequence_left, string& s) {
-  ll n = s.size();
-  ll current_zeros = 0;
-  ll current_ones = 0;
-  if (s[n - 1] == '0') {
-    max_sequence_left[n - 1].second = 1;
-    current_zeros = 1;
-  } else {
-    max_sequence_left[n - 1].first = 1;
-    current_ones = 1;
-  }
-  for (int i = n - 2; i >= 0; --i) {
-    if (s[i] == '0') {
-      current_ones = 0;
-      current_zeros += 1;
-      max_sequence_left[i].second = max(max_sequence_left[i + 1].second, current_zeros);
-    } else {
-      current_zeros = 0;
-      current_ones += 1;
-      max_sequence_left[i].first = max(max_sequence_left[i + 1].first, current_ones);
-    }
-  }
-}
-
 void solve() {
-  ll n, k; cin >> n >> k;
-  string s; cin >> s;
-  vector<pll> max_sequence_left(n, {0, 0});
-  vector<pll> max_sequence_right(n, {0, 0});
-  Count1(max_sequence_left, s);
-  Count2(max_sequence_right, s);  
-
-  for (int a = 1; a <= n; ++a) {
-    for (int l = 0; l < n; ++l) {
-      for (int r = l; r < n; ++r) {
-        if (r - l + 1 != k) {
-          continue;
-        }
-
-        
-
-      }
-    }
-  }
+  
 }
 
 int main() {
