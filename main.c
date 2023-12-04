117d0c7ac585a705e8909ffc013463009301ce97
Stepan Karpov <stepan.karpov@list.ru>
2023-12-05 00:23:27 +0300

    sdf

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <pthread.h>
+#include <unistd.h>
 
-int main ()
-{
-  uint16_t x = 0x0001;
-  printf("%s-endian\n", *((uint8_t *) &x) ? "little" : "big");
+int counter = 0;
+pthread_mutex_t mutex;
+
+void* routine() {
+  for (int i = 0; i < 1e8; ++i) {
+    pthread_mutex_lock(&mutex);
+    ++counter;
+    pthread_mutex_unlock(&mutex);
+  }
+}
+
+int main () {
+  pthread_t threads[8];
+
+  pthread_mutex_init(&mutex, NULL);
+
+  for (int i = 0; i < 8; ++i) {
+    pthread_create(&threads[i], NULL, routine, NULL);
+    printf("Thread %d starts\n", i);
+  }
+  for (int i = 0; i < 8; ++i) {
+    pthread_join(threads[i], NULL);
+    printf("Thread %d ends\n", i);
+  }
+  
+  pthread_mutex_destroy(&mutex);
+
+  printf("%d", counter);
+  return 0;
 }
