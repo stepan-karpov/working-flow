10a2a674b3e791c1dc717b75e8d914f0c53dede3
Stepan Karpov <stepan.karpov@list.ru>
2023-12-15 21:19:20 +0300

    sdf

--- gosha.py
+++ gosha.py
@@ -7,11 +7,11 @@ cnt_reshka = 0
 
 for i in range(0, 100000):
   value = random.randint(0, 10)
-
+ 
   if value == 1:
     cnt_orel += 1
   else:
     cnt_reshka += 1
   
 print(cnt_orel)
-print(cnt_reshka)
+print(cnt_reshka) 
