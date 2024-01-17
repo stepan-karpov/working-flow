2144199c99473ffb0b302e67f7a463cb1227e055
Stepan Karpov <stepan.karpov@list.ru>
2024-01-17 19:52:30 +0300

    python main upd

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-
-privet = "chem_mogu_pomoch_sevodnya"
-privetik = "privet-privet,_tolko_vas_vspominala,_eto_k_bogatstvy"
-da_ysh_o_chom_bodem_govorit = "ne_znayu"
-zdarova = "izvenite,_zagevala_plenky,_skoro_fse_pochinim"
-print("ii_nachiaet_raboty")
-print("privet")
+print()
