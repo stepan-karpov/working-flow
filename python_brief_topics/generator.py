6248e8fe21f8fb8a0beb6c21ef4e74c6044dd390
Stepan Karpov <stepan.karpov@list.ru>
2024-02-16 18:44:38 +0300

    generator

--- python_brief_topics/generator.py
+++ python_brief_topics/generator.py
@@ -1,17 +1,26 @@
-def my_generator():
-  for i in range(0, 15):
-    if (i % 4 == 0):
-      yield i
+# def my_generator():
+#   for i in range(0, 15):
+#     if (i % 4 == 0):
+#       yield i
 
-a = [x ** 2 for x in range(5)]
-print(a)
+# a = [x ** 2 for x in range(5)]
+# print(a)
 
-a = [x for x in my_generator()]
-print(a)
+# a = [x for x in my_generator()]
+# print(a)
 
-a = {x ** 2 for x in range(5)}
-print(a)
+# a = {x ** 2 for x in range(5)}
+# print(a)
 
 
-a = {x: x ** 2 for x in range(5)}
+# a = {x: x ** 2 for x in range(5)}
+# print(a)
+
+gen = (x for x in range(0, 10) if x % 2 == 0)
+
+print(next(gen))
+
+print(list(gen))
+
+a = {x ** 2 for x in range(-4, 5)}
 print(a)
