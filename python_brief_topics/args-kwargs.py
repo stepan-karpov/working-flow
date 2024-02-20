a9eb5af55ce60562cd5c1440a8319b58ad81b605
Stepan Karpov <stepan.karpov@list.ru>
2024-02-20 15:50:18 +0300

    args-kwargs

--- python_brief_topics/args-kwargs.py
+++ python_brief_topics/args-kwargs.py
@@ -4,3 +4,6 @@ def function(a: int = 1, *args, **kwargs):
   print(kwargs)
 
 function(34, 54, "s", 1.0, b=45, dog='haha')
+
+print(*[1, 4, 5, 6])
+print(**{"s": 5, "a": 2, "b": 4, "c": 6, "d": 3, "e": 2})
