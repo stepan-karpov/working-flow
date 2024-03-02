0bed939df0aa406dc56e4cfb749fb62f1530baa8
Stepan Karpov <stepan.karpov@list.ru>
2024-03-03 01:06:13 +0300

    temp

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-def foo(tmp=None):
-  if tmp is None:
-    tmp = []
-  tmp.append(1)
-  return tmp
+from collections import defaultdict
 
-def bar(tmp=()):
-  tmp += (1,)
-  return tmp
+def download_update(n, k):
+    parts = defaultdict(int)
+    devices = defaultdict(list)
+    
+    for i in range(1, k+1):
+        parts[i] = 0
+    
+    time_slots = 0
+    while True:
+        # Check if all devices have received all parts of the update
+        if all(len(devices[i]) == k for i in range(2, n+1)):
+            break
+        
+        for device_id in range(2, n+1):
+            min_part = min(parts.keys(), key=lambda x: (parts[x], x))
+            devices[device_id].append(min_part)
+            parts[min_part] += 1
+        
+        for device_id in range(2, n+1):
+            parts[devices[device_id].pop(0)] -= 1
+        
+        time_slots += 1
+    
+    return time_slots
 
-print(bar())
-print(bar())
-print(bar())
-print(bar())
+n, k = map(int, input().split())
+result = download_update(n, k)
+print(result)
