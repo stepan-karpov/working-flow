dd2c2cbadb2619d027f6fc4bcf1c0c3c4e1c32c2
Stepan Karpov <stepan.karpov@list.ru>
2024-03-02 14:49:37 +0300

    haha

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
 
-void Init() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkInclusion(string s1, string s2) {
+    std::unordered_map<char, int> window;
+    std::unordered_map<char, int> s1_map;
+    for (int i = 0; i < s1.size(); ++i) {
+        ++window[s2[i]];
+        ++s1_map[s1[i]];
+    }
+
+    if (s1_map == window) { return true; }
+
+    for (int i = s1.size(); i < s2.size(); ++i) {
+        --window[s2[i - s1.size()]];
+        if (window[s2[i - s1.size()]] == 0) {
+          window.erase(s2[i - s1.size()]);
+        }
+        ++window[s2[i]];
+        if (s1_map == window) { return true; }
+    }
+    return false;
 }
 
-struct A {
-  A() {
-    std::cout << "Constr\n";
-  }
-
-  ~A() {
-    std::cout << "Destr\n";
-  }
-};
-
 int main() {
-  Init();
-
-  std::vector<A> vv(4);
-
-  vv.clear();
-
-  std::cout << "sdf\n";
-
-
-  return 0;
+  std::cout << checkInclusion("ab", "eidbaooo") << "\n";
 }
