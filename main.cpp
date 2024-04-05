821dfd21c9cf3bd26b04e65d68230cf6dce80c3e
Stepan Karpov <stepan.karpov@list.ru>
2024-04-05 13:36:31 +0300

    main updated

--- main.cpp
+++ main.cpp
@@ -22,109 +22,26 @@ const string ALPH = "abcdefghijklmnopqrstuvwxyz";
 
 // v2 = rand() % 100 + 1;  --- v2 in the range 1 to 100
 
-bool graphFull(std::vector<std::vector<int>>& graph) {
-  for (int i = 0; i < graph.size(); ++i) {
-    for (int j = 0; j < graph[i].size(); ++j) {
-      if (graph[i][j] == 0) { return false; }
-    }
-  }
-  return true;
-}
-
-ll findSum(std::vector<int>& v) {
-  ll ans = 0;
-  for (int i = 0; i < v.size(); ++i) {
-    ans += v[i];
-  }
-  return ans;
-}
-
 void solve() {
-  ll n, k; cin >> n >> k;
-
-  std::vector<std::vector<int>> graph(n, std::vector<int>(k, 0));
-
-  for (int i = 0; i < k; ++i) {
-    graph[0][i] = 1;
-  }
-  std::vector<int> answer(n, -1);
-  std::vector<std::vector<int>> importance(n, std::vector<int>(n, 0));
-  answer[0] = 0;
-
-  int time = 1;
-
-  while (!graphFull(graph)) {
-    std::vector<int> downloaded(k, 0);
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < k; ++j) {
-        downloaded[j] += graph[i][j];
-      }
-    }
-
-    std::vector<int> to_download(n, -1);
-
-    for (int i = 0; i < n; ++i) {
-      if (answer[i] != -1) { continue; }
-
-      std::set<pll> absent;
-      for (int j = 0; j < k; ++j) {
-        if (graph[i][j] == 0) {
-          absent.insert({downloaded[j], j});
-        }
-      }
-      to_download[i] = absent.begin()->second;
-    }
-
-    std::vector<std::vector<int>> queries_from(n);
-
-    for (int i = 0; i < n; ++i) {
-      if (answer[i] != -1) { continue; }
-
-      int want_to_download = to_download[i];
-      std::set<pll> cand;
-      for (int j = 0; j < n; ++j) {
-        if (graph[j][want_to_download] == 0) { continue; }
-        cand.insert({findSum(graph[j]), j});
-      }
-      // query from i to cand.begin()->second
-      queries_from[cand.begin()->second].push_back(i);
-    }
-
-    std::vector<pll> queries_sat;
-
-    for (int i = 0; i < n; ++i) {
-      if (queries_from[i].size() == 0) { continue; }
-      std::set<pair<int, pll>> cand;
-      for (int to : queries_from[i]) {
-        cand.insert({importance[i][to], {findSum(graph[to]), to}});
-      }
-      queries_sat.push_back({i, cand.begin()->second.second});
-    }
-
-    for (pll t : queries_sat) {
-      int from = t.first, to = t.second;
-      importance[to][from]++;
-      graph[to][to_download[to]] = 1;
-      if (answer[to] == -1 && findSum(graph[to]) == k) {
-        answer[to] = time;
-      }
-      // std::cout << from << " -> " << to << " package " << to_download[to] << "\n";
-    }
-
-    // std::cout << "NEW EPOCH\n\n";
-    ++time;
-  }
-  for (int i = 1; i < n; ++i) {
-    std::cout << answer[i] << " ";
-  }
-  std::cout << "\n";
+  
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
-  solve();
+  ll t = 1;
+  // cin >> t;
+  // cout << fixed << setprecision(10);
+  
+  while (t--) {
+    solve();
+    // cout << solve() << endl;
+    // if (solve())
+    //    cout << "Yes" << endl;
+    // else
+    //    cout << "No" << endl;
+  }
 
   return 0;
 }
--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-import pandas as pd
-import numpy as np
-
-# Create a small example DataFrame
-data = {
-    'A': [1000, 2000, np.nan, 4000, 5000],
-    'B': [np.nan, 20, 30, np.nan, 50],
-    'C': [10, np.nan, 30, 40, 50]
-}
-
-df = pd.DataFrame(data)
-cols_to_fill = ['A', 'B', 'C']
-
-# Fill NaN values with the average value in each column
-df[cols_to_fill] = df[cols_to_fill].fillna(df[cols_to_fill].mean())
-
-print(df)
+ans = 0
+a = 123
